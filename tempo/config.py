603e0247c6e76a4e37fd4033329e1482d4cd6140
Stephen Kelly <stevekm@users.noreply.github.com>
2020-03-18 12:26:14 -0400

    add LSF env vars to config script

--- tempo/config.py
+++ tempo/config.py
@@ -2,19 +2,25 @@
 # -*- coding: utf-8 -*-
 """
 Creates an updated config file for the pipeline
-Save variables from the environment to JSON
-Meant to be run from inside the Makefile, to propagate env vars to file for downstream usage
+Save variables from the environment to JSON for downstream usage
+Meant to be run from inside the Makefile, optionally from inside LSF job from command
+make submit
 """
 import os
 import json
 
-CONFIG_JSON = os.environ['CONFIG_JSON'] # required
+# get environment variables
+# required:
+CONFIG_JSON = os.environ['CONFIG_JSON']
+# optional:
 TIMESTAMP = os.environ.get('TIMESTAMP')
 LOG_DIR = os.environ.get('LOG_DIR')
 NXF_LOG = os.environ.get('NXF_LOG')
 MAPPING_TSV = os.environ.get('MAPPING_TSV')
 PAIRING_TSV = os.environ.get('PAIRING_TSV')
 OUTPUT_DIR = os.environ.get('OUTPUT_DIR')
+LSB_JOBID = os.environ.get('LSB_JOBID')
+LSB_OUTPUTFILE = os.environ.get('LSB_OUTPUTFILE')
 
 config = {
 "timestamp": TIMESTAMP,
@@ -22,7 +28,9 @@ config = {
 "nextflow_log": NXF_LOG,
 "mapping_tsv": MAPPING_TSV,
 "pairing_tsv": PAIRING_TSV,
-"output_dir": OUTPUT_DIR
+"output_dir": OUTPUT_DIR,
+"lsf_jobid": LSB_JOBID,
+"lsf_log": LSB_OUTPUTFILE
 }
 
 
